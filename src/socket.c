0cf7b839172c7a475641452efdbda16b803ef43c
devTBenoth <devtborne@gmail.com>
2023-04-19 21:43:29 +0300

    GUI BACKEND (.c)

--- src/socket.c
+++ src/socket.c
@@ -8,51 +8,66 @@
 #include "functions.h"
 #define MAX_BUFFER_SIZE 10000
 
-int main(int argc, char **argv) {
-
-    int bufferSize = strtol(argv[3], NULL, 10);
-    if (bufferSize > MAX_BUFFER_SIZE) {
-        printf("Tampon boyutu %d'dan büyük olamaz.\n", MAX_BUFFER_SIZE);
-        return EXIT_FAILURE;
-    }
-
+int bufferSocket(int bufferSize, char *ip_address, int port, char *buffer) {
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketfd == -1) {
-        printf("Soket oluşturma hatası: %s\n", strerror(errno));
+        perror("socket");
         return EXIT_FAILURE;
     }
 
     struct sockaddr_in server;
     memset(&server, 0, sizeof(server));
-
     server.sin_family = AF_INET;
-    server.sin_port = htons(atoi(argv[2]));
+    server.sin_port = htons(port);
 
-    if (inet_pton(AF_INET, argv[1], &server.sin_addr) <= 0) {
-        printf("Geçersiz IP adresi: %s\n", argv[1]);
+    if (inet_pton(AF_INET, ip_address, &server.sin_addr) <= 0) {
+        perror("inet_pton");
         return EXIT_FAILURE;
     }
-    printf("IP ADDRESS --> %s\n", argv[1]);
-    printf("PORT NUMBER --> %s\n", argv[2]);
+    if (connect(socketfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+        perror("connect");
+        return EXIT_FAILURE;
+    }
 
-    
-        if (connect(socketfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
-            printf("Bağlantı hatası: %s\n", strerror(errno));
+    generate_buffer(buffer, bufferSize);
+    if (write(socketfd, buffer, bufferSize) == -1) {
+        perror("write");
+        return EXIT_FAILURE;
+    }
+    close(socketfd);
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+    if (argc <= 5) {
+        return EXIT_FAILURE;
+    }
+
+    int bufferSize = strtol(argv[3], NULL, 10);
+
+    char buffer[MAX_BUFFER_SIZE];
+    if (argv[5] != NULL) {
+        strcpy(buffer, argv[5]);
+    }
+
+    if (strcmp(argv[4], "y") == 0 || strcmp(argv[4], "Y") == 0) {
+        int x = 0;
+        while (1) {
+            x++;
+            if (bufferSocket(bufferSize, argv[1], atoi(argv[2]), buffer) == -1) {
+                return EXIT_FAILURE;
+            }
+                if (x == 10) {
+                    break;
+                }
+		bufferSize += 100;
+	}
+    }
+    else {
+        if (bufferSocket(bufferSize, argv[1], atoi(argv[2]), buffer) == -1) {
             return EXIT_FAILURE;
         }
-    
-        char buffer[MAX_BUFFER_SIZE];
-        if (argv[4] != NULL) {
-            strcpy(buffer, argv[4]);
-        }
-        generate_buffer(buffer, bufferSize);
-        printf("Buffer uzunluğu: %s\n", buffer);
-    
-        if (write(socketfd, buffer, bufferSize) == -1) {
-            printf("Yazma hatası: %s\n", strerror(errno));
-            return EXIT_FAILURE;
-        }
-        close(socketfd);
-
+    }
+    printf("%d\n", strlen(buffer));
     return EXIT_SUCCESS;
 }
